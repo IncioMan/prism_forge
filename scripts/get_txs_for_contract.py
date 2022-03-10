f790716c7d8751cfb827ac096452aa3494cfb8d6
Alex Incerti <a.incerti@iconsulting.biz>
2022-03-10 18:19:17 +0100

    avoid failed txs

--- scripts/get_txs_for_contract.py
+++ scripts/get_txs_for_contract.py
@@ -7,14 +7,17 @@ txs = requests.get(f"https://api.extraterrestrial.money/v1/txs/by_account?accoun
 limit = 100
 offset = 0
 tx_hashes = []
+i = 0
 while(True):
     url = f"https://api.extraterrestrial.money/v1/txs/by_account?account={contract}&order=desc&offset={offset}&limit={limit}"
     txs = requests.get(url).json()
     orig_size = len(tx_hashes)
     try:
-        tx_hashes = [*tx_hashes,*list(map(lambda x: (x['txhash'],x['timestamp']),txs['txs']))]
-        if(len(tx_hashes)%500==0):
+        i += len(txs['txs'])
+        tx_hashes = [*tx_hashes,*list(map(lambda x: (x['txhash'],x['timestamp']),filter(lambda x: 'code' not in x,txs['txs'])))]
+        if(i%500==0):
             print(len(tx_hashes))
+        if(i%2000==0):
             txs = pd.DataFrame(tx_hashes, columns=['hash','timestamp'])
             txs.to_csv(f'./data/txs/{contract}.csv')
         offset+=limit
